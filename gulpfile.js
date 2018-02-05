c8cb947374f7cca54c9b1d6c53d79dec5755d9d1
Shady Khalifa <shekohex@gmail.com>
2018-02-05 15:02:33 +0200

    chore(gulp): fix typo

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,6 @@ const source = 'src';
 const distId = process.argv.indexOf('--dist');
 const dist = distId < 0 ? 'node_modules/nest-router' : process.argv[distId + 1];
 
-gulp.task('default', function() {
-  modules.forEach(module => {
-    gulp.watch([`${source}/${module}/**/*.ts`, `${source}/${module}/*.ts`], [module]);
-  });
-});
-
 gulp.task('build', () => {
   return package
     .src()
