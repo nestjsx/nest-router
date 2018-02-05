2887ba172f037835dfc2e437b8aa12e8a16c8b92
Shady Khalifa <shekohex@gmail.com>
2018-02-05 14:45:13 +0200

    docs(src): add comments and some docs to the Public API

--- src/router.module.ts
+++ src/router.module.ts
@@ -2,8 +2,16 @@ import { Module, DynamicModule } from '@nestjs/common';
 import { validatePath } from './utils/validate-path.util';
 import { Routes } from './routes.interface';
 const MODULE_PATH = '__module_path__';
+/**
+ * A utility Module to Organize your Routes,
+ * it could be imported in the Root Module of you application.
+ */
 @Module({})
 export class RouterModule {
+  /**
+   * takes an array of modules and organize them in hierarchy way
+   * @param routes {Routes} Array of Routes
+   */
   public static forRoutes(routes: Routes): DynamicModule {
     RouterModule.buildPathMap(routes);
     return {
