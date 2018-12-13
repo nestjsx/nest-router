d7fae4f2b8b906bedaa737390ba8b4201853256d
Shady Khalifa <shekohex@gmail.com>
2018-12-13 12:04:08 +0200

    add pipes example.

--- examples/nest-v5x/src/cats/cats.controller.ts
+++ examples/nest-v5x/src/cats/cats.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, ValidationPipe } from '@nestjs/common';
+import { CreateCatDTO } from './dto/create-cat.dto';
 
 @Controller()
 export class CatsController {
+  private cats: string[] = [];
   @Get('/')
   sayHello() {
     return `Hello From CatsController`;
+  }
+
+  // For testing Pipes
+  @Post('/create')
+  public testing(@Body(new ValidationPipe()) data: CreateCatDTO): string[] {
+    this.cats.push(data.name);
+    return this.cats;
   }
 }
